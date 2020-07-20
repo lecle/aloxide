e74090e0ee9e02c81307c06f2a4ecf70efff241c
manh <manh.vv.htth@gmail.com>
2020-07-20 17:47:51 +0700

    feat(setup): :egg: setup lerna

---
+++ packages/bar/src/index.ts
@@ -0,0 +1,4 @@
+import { meaningOfLife } from '@aloxide/foo';
+
+// eslint-disable-next-line no-console
+console.log(meaningOfLife);
---
+++ packages/jest-tsjest/src/index.ts
@@ -0,0 +1,3 @@
+import { meaningOfLife } from '@aloxide/foo';
+
+export const whatIsTheMeaningOfLife = () => meaningOfLife;
