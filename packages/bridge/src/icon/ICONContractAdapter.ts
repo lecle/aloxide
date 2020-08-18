6c1cc7bbf6f42e22f2519fcd3cc55c1d73c9a20f
Quoc Le <songmai108@gmail.com>
2020-08-18 16:41:02 +0700

    fix(birdge): use lower;5D case for method in ICON

--- packages/bridge/src/icon/ICONContractAdapter.ts
+++ packages/bridge/src/icon/ICONContractAdapter.ts
@@ -74,9 +74,18 @@ export class ICONContractAdapter extends AbsContractAdapter {
     const template = Handlebars.compile(hbsTemplate);
 
     const outText = template({
+<<<<<<< HEAD
       tables: this.entityConfigs?.map(item => {
+=======
+      tables: this.entityConfigs ?.map(item => {
+        let key= item.fields.find(({ name }) => name == item.key);
+>>>>>>> fix(birdge): use lower;5D case for method in ICON
         return {
-          name: item.name,
+          name: item.name.toLocaleLowerCase(),
+          key: {
+            name: key.name,
+            type: this.typeInterpreter.interpret(key.type as FieldTypeEnum)
+          },
           fields: item.fields.map(({ name, type }) => ({
             name,
             type: this.typeInterpreter.interpret(type as FieldTypeEnum),
@@ -94,12 +103,26 @@ export class ICONContractAdapter extends AbsContractAdapter {
       'utf-8',
     );
     const template = Handlebars.compile(hbsTemplate);
+<<<<<<< HEAD
 
     this.entityConfigs?.map(item => {
       const actionName = 'get' + item.name;
       const outFileName = actionName + '.json';
       const outText = template({
         action: actionName.toLocaleLowerCase(),
+=======
+    
+    this.entityConfigs ?.map(item => {
+      const actionName = "get" + item.name
+      const outFileName = actionName + ".json";
+      let key= item.fields.find(({ name }) => name == item.key);
+      const outText = template({
+        key: {
+          name: key.name,
+          type: this.typeInterpreter.interpret(key.type as FieldTypeEnum)
+        },
+        action: actionName.toLocaleLowerCase()
+>>>>>>> fix(birdge): use lower;5D case for method in ICON
       });
       this.jsFilePrinter.print(outFileName, outText, this.folderName);
     });
@@ -139,11 +162,28 @@ export class ICONContractAdapter extends AbsContractAdapter {
       this.jsFilePrinter.print(uptFileName, uptText, this.folderName);
 
       // remove data
+<<<<<<< HEAD
       const rmvActionName = 'rmv' + item.name;
       const rmvFileName = rmvActionName + '.json';
       const rmvText = template({
         action: rmvActionName.toLocaleLowerCase(),
         fields: [{ name: 'id', type: 'int' }],
+=======
+      const rmvActionName = "rmv" + item.name
+      const rmvFileName = rmvActionName + ".json";
+      var indexFields = item.fields.reduce((ids, field) => {
+        if (field.name === item.key) {
+          ids.push(field);
+        }
+        return ids;
+      }, []);
+      const rmvText = template({
+        action: rmvActionName.toLocaleLowerCase(),
+        fields: indexFields.map(({ name, type }) => ({
+          name,
+          type: this.typeInterpreter.interpret(type as FieldTypeEnum),
+        })),
+>>>>>>> fix(birdge): use lower;5D case for method in ICON
       });
       this.jsFilePrinter.print(rmvFileName, rmvText, this.folderName);
     });
