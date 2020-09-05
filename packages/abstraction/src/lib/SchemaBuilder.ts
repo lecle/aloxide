792062b24a5315fead0034846f5c00a9f119c595
manh <manh.vv.htth@gmail.com>
2020-09-05 15:07:16 +0700

    fix(abstraction): :bug: fix case of letter in `utils`

--- packages/abstraction/src/lib/SchemaBuilder.ts
+++ packages/abstraction/src/lib/SchemaBuilder.ts
@@ -1,24 +1,24 @@
-import { loadContent } from './utils'
+import { loadContent } from './Utils';
 
 const TYPE_MAPPER = {
-    'string': String,
-    'number': Number,
-    'boolean': Boolean,
-}
-export default class SchemaBuilder {
-    
-    static getSchema(schema) {
-        const schemaJson = typeof schema === 'string' ? loadContent(schema) : schema
-        return SchemaBuilder._convertToJS(schemaJson)
-    }
-
-    private static _convertToJS(object) {
-        let result = object instanceof Array ? [] : {}
-        for (let key in object) {
-            const isObject = typeof object[key] === 'object'
-            result[key] = isObject ? SchemaBuilder._convertToJS(object[key]) : (TYPE_MAPPER[object[key]] || object[key])
-        }
-        return result;
-    }
+  string: String,
+  number: Number,
+  boolean: Boolean,
 };
+export default class SchemaBuilder {
+  static getSchema(schema) {
+    const schemaJson = typeof schema === 'string' ? loadContent(schema) : schema;
+    return SchemaBuilder._convertToJS(schemaJson);
+  }
 
+  private static _convertToJS(obj) {
+    const result = obj instanceof Array ? [] : {};
+    for (const key in obj) {
+      const isObject = typeof obj[key] === 'object';
+      result[key] = isObject
+        ? SchemaBuilder._convertToJS(obj[key])
+        : TYPE_MAPPER[obj[key]] || obj[key];
+    }
+    return result;
+  }
+}
