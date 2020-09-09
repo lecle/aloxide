710e4da469a46864c80088723a20d0fc4361be80
manhvu <manhvu@orgos.net>
2020-09-09 11:59:05 +0700

    feat(example-api-gateway): :sparkles: add watcher on iconloop

--- packages/example-api-gateway/src/config.ts
+++ packages/example-api-gateway/src/config.ts
@@ -7,30 +7,28 @@ declare global {
   var _gSequelize: Sequelize;
 }
 
-function connectDb() {
-  if (!global._gSequelize) {
-    global._gSequelize = new Sequelize(
-      process.env.app_postgres_db,
-      process.env.app_postgres_user,
-      process.env.app_postgres_pw,
-      {
-        host: process.env.app_postgres_host,
-        port: parseInt(process.env.app_postgres_port, 10),
-        dialect: 'postgres',
-      },
-    );
+export function connectDb(db?: string) {
+  const gSequelize = new Sequelize(
+    db || process.env.app_postgres_db,
+    process.env.app_postgres_user,
+    process.env.app_postgres_pw,
+    {
+      host: process.env.app_postgres_host,
+      port: parseInt(process.env.app_postgres_port, 10),
+      dialect: 'postgres',
+    },
+  );
 
-    global._gSequelize
-      .authenticate()
-      .then(() => {
-        logger.debug(`Connection to database has been established successfully.`);
-      })
-      .catch(err => {
-        logger.error(`Unable to connect to the database:`, err);
-      });
-  }
+  gSequelize
+    .authenticate()
+    .then(() => {
+      logger.debug(`Connection to database has been established successfully.`);
+    })
+    .catch(err => {
+      logger.error(`Unable to connect to the database:`, err);
+    });
 
-  return global._gSequelize;
+  return gSequelize;
 }
 
 export default {
