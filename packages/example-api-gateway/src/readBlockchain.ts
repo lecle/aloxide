9de0ae6bd5f8c568e7d21efd3d4606f0e588a81d
manhvu <manhvu@orgos.net>
2020-09-09 11:59:05 +0700

    feat(example-api-gateway): :sparkles: add watcher on iconloop

--- packages/example-api-gateway/src/readBlockchain.ts
+++ packages/example-api-gateway/src/readBlockchain.ts
@@ -1,14 +1,10 @@
 import { createWatcher } from '@aloxide/demux';
+import { IconActionReader } from '@aloxide/demux-icon';
 import { NodeosActionReader } from 'demux-eos';
 
-import config from './config';
+import config, { connectDb } from './config';
 
-const actionReader = new NodeosActionReader({
-  nodeosEndpoint: process.env.app_nodeosEndpoint,
-  onlyIrreversible: false,
-  startAtBlock: parseInt(process.env.app_startAtBlock, 10),
-});
-
+// watch EOS
 createWatcher({
   accountName: process.env.app_d_eos_account_name,
   modelBuilderConfig: {
@@ -16,16 +12,56 @@ createWatcher({
     logger: config.logger,
   },
   sequelize: config.sequelize,
-  actionReader,
+  actionReader: new NodeosActionReader({
+    nodeosEndpoint: process.env.app_nodeosEndpoint,
+    onlyIrreversible: false,
+    startAtBlock: parseInt(process.env.app_startAtBlock, 10),
+  }),
   actionWatcherOptions: {
     pollInterval: 5000,
     logLevel: 'debug',
-    logSource: 'ReadBlockChain',
+    logSource: 'sync-EOS',
   },
 })
   .then(actionWatcher => {
-    actionWatcher.watch();
+    if (process.env.app_enable_eos == 'true') {
+      logger.info('EOS enabled');
+      actionWatcher.watch();
+    }
   })
   .catch(err => {
     logger?.error('---- createWatcher error:', err);
+  });
+
+// watch ICON
+createWatcher({
+  accountName: 'cxd1c341cba5d21f5c1ea36bade8369270a2fe065c',
+  modelBuilderConfig: {
+    aloxideConfigPath: config.aloxideConfigPath,
+    logger: config.logger,
+  },
+  sequelize: connectDb(process.env.app_postgres_db_icon),
+  actionReader: new IconActionReader({
+    endpoint: process.env.app_icon_endpoint,
+    nid: parseInt(process.env.app_icon_nid, 10),
+    logLevel: 'info',
+    logSource: 'reader-ICON',
+    startAtBlock: 6536668 /* https://bicon.tracker.solidwallet.io/block/6536668 */,
+    numRetries: 5,
+    waitTimeMs: 1000,
+  }),
+  actionWatcherOptions: {
+    pollInterval: 2000,
+    logLevel: 'debug',
+    logSource: 'sync-ICON',
+  },
+})
+  .then(actionWatcher => {
+    if (process.env.app_enable_icon == 'true') {
+      logger.info('ICON enabled');
+      actionWatcher.watch();
+    }
+  })
+  .catch(err => {
+    logger.error('---- createWatcher error:', err);
   });
