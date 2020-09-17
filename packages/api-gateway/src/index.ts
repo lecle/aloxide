b4e4a768e881f1455c9165e1b18d88ca136018cd
manhvu <manhvu@orgos.net>
2020-09-17 16:08:34 +0700

    refactor(api-gateway): :fire: remove dependency on @aloxide/logger, @aloxide/model, sequelize
    * add dependency on @aloxide/demux
    
    * change in CreateRouterConfig
    * change in CreateGraphQlConfig

--- packages/api-gateway/src/index.ts
+++ packages/api-gateway/src/index.ts
@@ -1,6 +1,7 @@
 export type { CreateRouterConfig } from './express-router';
-export { createRouter } from './express-router';
-
 export type { CreateGraphQlConfig, CreateGraphQlOutput } from './graphql-relay';
+
+export { createRouter } from './express-router';
 export { createGraphQl } from './graphql-relay';
+
 export { GraphqlTypeInterpreter } from './GraphqlTypeInterpreter';
