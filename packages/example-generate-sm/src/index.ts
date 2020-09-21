bda84fa3a9abbfcf994f58a72ca2d6b50e5ffd5d
manhvu <manhvu@orgos.net>
2020-09-21 13:38:22 +0700

    feat(example-generante-sm): :ambulance: add example of generate smart-contract no-state-data

--- packages/example-generate-sm/src/index.ts
+++ packages/example-generate-sm/src/index.ts
@@ -1,15 +1,21 @@
 import { ContractGenerator } from '@aloxide/abstraction';
-import { EOSContractAdapter } from '@aloxide/bridge';
+import { EOSContractAdapter, ICONContractAdapter, ModelContractAdapter } from '@aloxide/bridge';
 import { createLogger } from 'bunyan';
 import path from 'path';
 
+const resultPath = path.resolve(__dirname, '../out');
+
 const contractGenerator = new ContractGenerator({
   aloxideConfigPath: path.resolve(__dirname, '../samples/aloxide.yml'),
-  resultPath: path.resolve(__dirname, '../out'),
+  resultPath,
   adapters: [
-    new EOSContractAdapter(true) /* generate smart contract for EOS network */,
-    // new ICONContractAdapter(),
-    // new ModelContractAdapter(),
+    new EOSContractAdapter() /* generate smart contract for EOS network */,
+    new EOSContractAdapter({
+      logDataOnly: true,
+      outputPath: path.resolve(resultPath, 'eos-no-state'),
+    }) /* generate smart contract for EOS network */,
+    new ICONContractAdapter(),
+    new ModelContractAdapter(),
   ],
   logger: createLogger({
     level: 'debug',
