be4eb7c95b39c60cf85527405bd544d7ef3932ba
quyvo <quyvo@lecle.co.kr>
2020-09-22 16:54:31 +0700

    feat/#8 write data type between ledgers

--- packages/api-gateway/src/GraphqlTypeInterpreter.ts
+++ packages/api-gateway/src/GraphqlTypeInterpreter.ts
@@ -1,15 +1,24 @@
 import { FieldTypeEnum, Interpreter } from '@aloxide/bridge';
-import { GraphQLInt, GraphQLScalarType, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLScalarType, GraphQLString } from 'graphql';
 
 export class GraphqlTypeInterpreter implements Interpreter<FieldTypeEnum, GraphQLScalarType> {
   interpret(input: FieldTypeEnum): GraphQLScalarType {
     let type: GraphQLScalarType;
 
     switch (input) {
-      case FieldTypeEnum.number:
+      case FieldTypeEnum.uint16_t:
+      case FieldTypeEnum.uint32_t:
       case FieldTypeEnum.uint64_t:
         type = GraphQLInt;
         break;
+      case FieldTypeEnum.number:
+      case FieldTypeEnum.double:
+        type = GraphQLFloat;
+        break;
+      case FieldTypeEnum.bool:
+        type = GraphQLBoolean;
+        break;
+      case FieldTypeEnum.account:
       case FieldTypeEnum.string:
         type = GraphQLString;
         break;
--- packages/bridge/src/eos/EOSTypeInterpreter.ts
+++ packages/bridge/src/eos/EOSTypeInterpreter.ts
@@ -7,11 +7,26 @@ export class EOSTypeInterpreter extends AbsTypeInterpreter {
 
     switch (type) {
       case FieldTypeEnum.number:
+      case FieldTypeEnum.double:
+        mappedType = 'double';
+        break;
+      case FieldTypeEnum.uint16_t:
+        mappedType = 'uint16_t';
+        break;
+      case FieldTypeEnum.uint32_t:
+        mappedType = 'uint32_t';
+        break;
       case FieldTypeEnum.uint64_t:
         mappedType = 'uint64_t';
         break;
       case FieldTypeEnum.string:
         mappedType = 'std::string';
+        break;
+      case FieldTypeEnum.bool:
+        mappedType = 'bool';
+        break;
+      case FieldTypeEnum.account:
+        mappedType = 'eosio::name';
         break;
       default:
         throw new Error(`unknow type ${type}`);
