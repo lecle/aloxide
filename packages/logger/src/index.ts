c926af1eb2601e389237c21d77a3e756bbd4ad07
manhvu <manhvu@orgos.net>
2020-09-22 13:25:56 +0700

    fix(logger): :white_check_mark: fix unit test

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,38 +1,21 @@
-import chalk from 'chalk';
-
 import { Logger } from './Logger';
 
-export * from './Logger';
+export { Logger } from './Logger';
 
-// tslint:disable:no-console
-const log = (title, color = 'blue', ...p) =>
-  console.log(`${chalk[color].bold(title)} : ${chalk[color](...p)}`);
+export function createLogger({ consoleLogger } = { consoleLogger: false }): Logger {
+  let createLog;
 
-export function createLogger(
-  {
-    consoleLogger,
-  }: {
-    consoleLogger: boolean;
-  } = {
-    consoleLogger: false,
-  },
-): Logger {
-  let logger: Logger;
   if (consoleLogger) {
-    logger = {
-      debug: (...p) => log('DEBUG', 'blue', ...p),
-      info: (...p) => log('INFO', 'blue', ...p),
-      warn: (...p) => log('WARNING', 'yellow', ...p),
-      error: (...p) => log('ERROR', 'red', ...p),
-    };
+    const chalk = require('chalk');
+    createLog = (level: string, color: string) => (...p) => console[level](chalk[color](...p));
   } else {
-    const nullLogger = (...p) => {};
-    logger = {
-      debug: nullLogger,
-      info: nullLogger,
-      warn: nullLogger,
-      error: nullLogger,
-    };
+    createLog = () => () => {};
   }
-  return logger;
+
+  return {
+    debug: createLog('debug', 'blue'),
+    info: createLog('info', 'green'),
+    warn: createLog('warn', 'yellow'),
+    error: createLog('error', 'red'),
+  };
 }
