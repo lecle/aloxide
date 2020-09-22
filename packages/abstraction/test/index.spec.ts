47921ab1ad650ae974c0ea77730b04e03495c5c5
manhvu <manhvu@orgos.net>
2020-09-22 12:45:04 +0700

    test(abstraction): :art: groom unit test coverage

--- packages/abstraction/test/index.spec.ts
+++ packages/abstraction/test/index.spec.ts
@@ -1,7 +1,11 @@
-import { ContractGenerator } from '../src';
+import { ContractGenerator, readAloxideConfig } from '../src';
 
 describe('testing index exports', () => {
   it('create new instance of ContractGenerator without config', () => {
     expect(() => new ContractGenerator(null)).toThrowError('Missing configuration!');
+  });
+
+  it('export readAloxideConfig', () => {
+    expect(typeof readAloxideConfig).toEqual('function');
   });
 });
