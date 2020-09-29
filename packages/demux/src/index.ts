f09a93c5a458fe7d253ff61385308d2cb6aa7ef0
manhvu <manhvu@orgos.net>
2020-09-29 15:33:51 +0700

    feat(demux): :sparkles: export BaseHandlerVersion

--- packages/demux/src/index.ts
+++ packages/demux/src/index.ts
@@ -9,6 +9,7 @@ export type { Logger } from './Logger';
 export type { DbUpdaterOptions } from './DbUpdater';
 export type { DbUpdaterActionPayload } from './DbUpdater';
 
+export { BaseHandlerVersion } from './BaseHandlerVersion';
 export { AloxideActionHandler } from './AloxideActionHandler';
 export { DbUpdater } from './DbUpdater';
 export { AloxideDataManager } from './AloxideDataManager';
