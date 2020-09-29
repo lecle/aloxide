b6b399ffb35eebb5ed6b0d3ad2618f2427003cf8
quyvo <quyvo@lecle.co.kr>
2020-09-29 14:37:05 +0700

    feat(demux): :sparkles: #89 add the payload to metadata so that we can get

--- packages/demux/src/DMeta.ts
+++ packages/demux/src/DMeta.ts
@@ -1,9 +1,11 @@
 import type { EntityConfig } from '@aloxide/bridge';
 import type { BlockInfo } from 'demux';
 import type { AloxideActionHandlerContext } from './AloxideActionHandler';
+import { DbUpdaterActionPayload } from './DbUpdater';
 
 export type DMeta = {
   entity: EntityConfig;
   blockInfo?: BlockInfo;
   context?: AloxideActionHandlerContext;
+  payload?: DbUpdaterActionPayload;
 };
