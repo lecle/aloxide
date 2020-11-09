a9621e7bb2760c2f1453a69adefc8c67b4080d30
manh <manh.vv.htth@gmail.com>
2020-11-09 16:39:33 +0700

    feat(bridge): :sparkles: add metadata to field
    we can optimize string field by add length

--- packages/bridge/src/type-definition/Field.ts
+++ packages/bridge/src/type-definition/Field.ts
@@ -1,6 +1,21 @@
 import { FieldTypeEnum } from './FieldTypeEnum';
 
+type FieldType = string | FieldTypeEnum;
+
 export interface Field {
-  type: string | FieldTypeEnum;
+  type: FieldType;
   name: string;
+  meta?: {
+    /**
+     * Example: given type is string and meta.type is tiny
+     * Then sequelize model datatype will be DataTypes.TEXT('tiny')
+     */
+    type?: string;
+
+    /**
+     * Example: given type is sring and meta.length is 64
+     * Then sequelize model datatype will be DataTypes.STRING(64)
+     */
+    length?: number;
+  };
 }
